380177fcb21d21fc3ff85110449bcea14395b064
unknown <khushivishwas8@gmail.com>
2026-06-17 08:47:10 +0530

    Fix missing brace bug, add palindrome and binary search

--- src/main/java/com/urlshortener/DSA.java
+++ src/main/java/com/urlshortener/DSA.java
@@ -1,7 +1,7 @@
 package com.urlshortener;
 
 public class DSA {
-    public static int countNumber(int[]arr1, int target){
+    public static int countNumber(int[] arr1, int target) {
         //time = o(1)
         //space = o(1)
         int count = 0;
@@ -11,45 +11,79 @@ public class DSA {
             }
         }
         return count;
-     }
+    }
 
-  public static int  linearSearch(int[]num,int target){
-      //time =o(n)---> not sure maybe be because it grows exactly with size of input
-      //space = o(1)
+    public static int linearSearch(int[] num, int target) {
+        //time =o(n)---> not sure maybe be because it grows exactly with size of input
+        //space = o(1)
         int index = 0;
-        for(int i = 0; i<num.length;i++){
-            if(num[i] == target){
-                 return i;
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == target) {
+                return i;
             }
         }
-    return -1;
- }
+        return -1;
+    }
+    //pallindrome
+    public static boolean isPalindrome(int[] arr7) { // method
+
+        int i = 0;
+        int j = arr7.length - 1;
+
+        while (i < j) {
+            if (arr7[i] != arr7[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 
 
-public static void main(String[] args){
+    // binary search
+
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+    public static void main(String[] args) {
 // find the largest number in the given block
-      // time= o(n)--> as it start from [0] but nested loop is also there and linear time -> because it check every
-    //   element .
-    //space=o(1)
-            int[] number1 = {3, 7, 1, 9, 4};
-            int largest1 = number1[0];
+        // time= o(n)--> as it start from [0] but nested loop is also there and linear time -> because it check every
+        //   element .
+        //space=o(1)
+        int[] number1 = {3, 7, 1, 9, 4};
+        int largest1 = number1[0];
 
-            for (int nums : number1) {
-                if (nums > largest1) {
-                    largest1 = nums;
-
-                }
+        for (int nums : number1) {
+            if (nums > largest1) {
+                largest1 = nums;
 
             }
+        }
+
             System.out.println("The largest number is:" + largest1);
 
             int[] number2 = {15, 2, 8, 15, 6};
-
             int largest2 = number2[0];
-
-            for (int nums : number2) {
-                if (nums > largest2) {
-                    largest2 = nums;
+            for (int num : number2) {
+                if (num > largest2) {
+                    largest2 = num;
                 }
             }
 
@@ -57,8 +91,8 @@ public static void main(String[] args){
 
 
             //sum
-    //time =o(1)
-    //space = o(1)
+            //time =o(1)
+            //space = o(1)
             int[] number = {1, 2, 3, 4, 5};
             int sum = 0;  // start sum from 0
             for (int num : number) {
@@ -75,7 +109,6 @@ public static void main(String[] args){
                 if (num < smallest) {   // take the current number.
                     smallest = num; // update the smallest number
                 }
-
             }
             System.out.println("Smallest number is " + smallest);
 
@@ -87,7 +120,7 @@ public static void main(String[] args){
             int j = arr.length - 1;// last index of the array
 
 
-            while (i < j) {// ne to swap 1st with the last until we reach at the middle
+            while (i < j) {// to swap 1st with the last until we reach at the middle
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
@@ -96,14 +129,13 @@ public static void main(String[] args){
             }
             //print reversed array;
             for (int num : arr) {
-
                 System.out.println(num + " ");
             }
 
 
             // loop and counting
-    //time =
-    //space = 0(1)
+            //time =
+            //space = 0(1)
             int[] arr2 = {1, 2, 1, 3, 1, 4};
             int result = countNumber(arr2, 2);
             System.out.println("count " + result);
@@ -113,18 +145,31 @@ public static void main(String[] args){
             System.out.println("count " + result);
 
 
-             //LinerSearch
+            //LinerSearch
 
-    int []arr0= {10, 20, 30, 40};
-             result = (linearSearch(arr0,30));
-    System.out.println("Index " + result);
+            int[] arr0 = {10, 20, 30, 40};
+            result = (linearSearch(arr0, 30));
+            System.out.println("Index " + result);
 
-    int[] arr5 = {10, 20, 30, 40};
-     result = (linearSearch(arr5,90));
-    System.out.println("Index " + result);
+            int[] arr5 = {10, 20, 30, 40};
+            result = (linearSearch(arr5, 90));
+            System.out.println("Index " + result);
 
-    int[] arr6 = {5, 15, 25};
-    result = (linearSearch(arr6,30));
-    System.out.println("Index " + result);
+            int[] arr6 = {5, 15, 25};
+            result = (linearSearch(arr6, 30));
+            System.out.println("Index " + result);
+
+            //palindrome
+            int[] array = {1, 2, 3, 2, 1};
+            boolean check = isPalindrome(array);
+            System.out.println("Is palindrome: " + check);
+
+            //binarySearch
+            int[] arr8 = {10, 20, 30, 40, 50};
+            int searchResult = binarySearch(arr8, 30);
+            System.out.println("Index " + searchResult);
+        }
+
+
     }
-     }
+}
